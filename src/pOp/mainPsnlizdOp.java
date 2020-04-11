dc9e892f1628b9ca7e501703580888b151d99189
aldridgevg <caxuser@live.com>
2020-04-11 23:14:01 +0800

    fully designed, with null pointer exception

--- src/pOp/mainPsnlizdOp.java
+++ src/pOp/mainPsnlizdOp.java
@@ -5,16 +5,54 @@ import java.util.Scanner;
 public class mainPsnlizdOp {
 
     public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+
         //get n ENSURES n<=10000
         int n = -1;
         while (n>10000 || n<0){
-            Scanner in = new Scanner(System.in);
             n = in.nextInt();
         }
+        System.out.println("n = "+n);
 
+        //init array
         numElem[] N = new numElem[n];
+        System.out.println("array initialization successful");
+        int end = 1;
+        int x = 0;
+        int y = 0;
 
+        in.nextLine();
+        //loop op
+        while(end != 0) {
 
+            //input command
+            String commandOp = in.nextLine();
+            System.out.println("ur command: "+commandOp);
+
+            //analyze command
+            String splitOp[] = commandOp.split(" ");
+            System.out.println(splitOp[0]+splitOp[1]);
+
+            end = new Integer(splitOp[0]).intValue();
+            if(end == 0) break;
+
+            else if(end == 1){
+                x = new Integer(splitOp[1]).intValue();
+                y = new Integer(splitOp[2]).intValue();
+                numOp.op1(x,y,N);
+            }
+
+            else if(end == 2){
+                x = new Integer(splitOp[1]).intValue();
+                numOp.op2(x,N);
+            }
+
+            else if(end == 3){
+                x = new Integer(splitOp[1]).intValue();
+                System.out.println(numOp.op3(x,N));
+            }
+        }
+        return;
     }
 
 }
--- src/pOp/numOp.java
+++ src/pOp/numOp.java
@@ -2,14 +2,7 @@ package pOp;
 
 public class numOp {
 
-    public void myPOp(int i){
-        switch (i){
-            case 1:
-
-        }
-    }
-
-    public void op1(int x, int y, numElem[] N){
+    public static void op1(int x, int y, numElem[] N){
 
         //if x has no child, remove x and set 0, attach to y head;
         if(N[x].head.nxt == null){
@@ -29,6 +22,7 @@ public class numOp {
 
             //find tail of x's linked list
             numNode tmp = N[x].head.nxt;
+            N[x].num = 0;
             N[x].head.num = y;
             N[x].head.nxt = null;
             while(tmp.nxt != null){
@@ -39,19 +33,46 @@ public class numOp {
             tmp.nxt = N[y].head.nxt;
             N[y].head.nxt = N[x].head.nxt;
         }
-
         return;
     }
 
-    public void op2(int x, numElem[] N){
+    public static void op2(int x, numElem[] N){
 
         //if x is a single set
-        if(N[x].head.num == x || N[x].head.nxt == null){
+        if(N[x].num == x || N[x].head.nxt == null){
             return;
         }
-        else if(N[x].head.nxt != null){
-            
+
+        //if x is in other set(including x)
+        else if(N[x].num == 0){
+
+            //delete x from origin list
+            int xold = N[x].head.num;
+            numNode p = N[xold].head;
+            numNode q = p;
+            while(p.num!=x){
+                q = p;
+                p = p.nxt;
+            }
+            //found x-node as p
+            q.nxt = p.nxt;
+            p = null;
+
+            //restore x
+            N[x].num = x;
+            N[x].head = new numNode(x);
         }
     }
 
+    public static int op3(int x, numElem[] N){
+
+        int xgrp = N[x].head.num;
+        int count = 0;
+        numNode p = N[xgrp].head;
+        while(p.nxt!=null){
+            count++;
+            p = p.nxt;
+        }
+        return count;
+    }
 }
